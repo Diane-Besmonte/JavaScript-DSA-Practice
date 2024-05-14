86ab0fa60fe04e1898afd97ef9bc351ef0ade6b2
Diane <elsiebesmonte@gmail.com>
2024-05-14 23:59:45 +0800

    Algorithm 1-5 lesson codes and notes

---
+++ Algorithm 1-5 Lesson.js
@@ -0,0 +1,28 @@
+// Big-O Notation
+function summation(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += 1;
+  }
+  return sum;
+}
+// Note: As the size of input increases, the time complexity also increases
+
+function summation(n) {
+  return (n * (n + 1)) / 2;
+}
+
+// Objects and Arrays Big-O
+
+// Objects
+const person = {
+  firstName: "Jett",
+  lastName: "Black",
+};
+
+// Object.keys()
+// Object.values()
+// Object.entries()
+
+// Arrays
+const odd = [1, 3, 5, 7, 9];

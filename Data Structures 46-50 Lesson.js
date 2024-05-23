d073a682b12350f58054ae23051b4bffcb69a63d
Diane <elsiebesmonte@gmail.com>
2024-05-23 23:49:02 +0800

    Added code from topics circular queue implementation to linked list print

---
+++ Data Structures 46-50 Lesson.js
@@ -0,0 +1,151 @@
+// *** Circular Queue Implementation
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.rear = -1;
+    this.front = -1;
+  }
+
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+
+  isEmpty() {
+    return (this.currentLength = 0);
+  }
+
+  enqueue(element) {
+    if (!this.isFull()) {
+      this.rear = (this.rear + 1) % this.capacity;
+      this.items[this.rear] = element;
+      this.currentLength += 1;
+      if (this.front === -1) {
+        this.front = this.rear;
+      }
+    }
+  }
+
+  dequeue(element) {
+    if (this.isEmpty()) {
+      return null;
+    }
+    const item = this.items[this.front];
+    this.items[this.front] = null;
+    this.front = (this.front + 1) % this.capacity;
+    this.currentLength -= 1;
+    if (this.isEmpty()) {
+      this.front = -1;
+      this.rear = -1;
+    }
+    return item;
+  }
+
+  peek() {
+    if (!this.isEmpty()) {
+      return this.items[this.front];
+    }
+    return null;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("Queue is Empty");
+    } else {
+      let i;
+      let str = "";
+      for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+        str += this.items[i] + " ";
+      }
+      str += this.items[i];
+      console.log(str);
+    }
+  }
+}
+
+const queue = new CircularQueue(5);
+console.log(queue.isEmpty());
+
+queue.enqueue(10);
+queue.enqueue(20);
+queue.enqueue(30);
+queue.enqueue(40);
+queue.enqueue(50);
+console.log(queue.isFull());
+queue.print();
+console.log(queue.dequeue(10));
+console.log(queue.peek());
+queue.print();
+queue.enqueue(60);
+queue.print();
+
+// *** Linked List
+// - is a linear data structure that includes a series of connected nodes
+
+// The linked list data structure supports three main operations
+// - Insertion - to add an element at the beginning, end or at a given index in the list
+// - Deletion - to remove an item given its index or value
+// - Search - to find an element given its value
+
+// Linked list usage
+// - All applications of both stacks and queues are applications of linked lists
+// - Image viewer
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  isEmpty() {
+    return this.size === 0;
+  }
+
+  getSize() {
+    return this.size;
+  }
+
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.size++;
+  }
+
+  print() {
+    if (this.isEmpty()) {
+      console.log("The list is empty");
+    } else {
+      let curr = this.head;
+      let listValues = "";
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
+}
+
+const list = new LinkedList();
+console.log("List is Empty?", list.isEmpty());
+console.log("List size ", list.getSize());
+list.print();
+list.prepend(10);
+list.print();
+
+list.prepend(20);
+list.prepend(30);
+list.print();
